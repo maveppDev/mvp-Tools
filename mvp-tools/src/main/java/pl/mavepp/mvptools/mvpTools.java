66c9c75242058c25a80587c691aaa7eada552374
maveppDev <mavepp1337@gmail.com>
2014-07-05 18:57:07 +0200

    First Update
    First....

---
+++ mvp-tools/src/main/java/pl/mavepp/mvptools/mvpTools.java
@@ -0,0 +1,21 @@
+package pl.mavepp.mvptools;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author maveppDev
+ * Copyright 2014 | maveppDev.net/Bukkit
+ * 
+ */
+public class mvpTools extends JavaPlugin{
+    @Override
+    public void onEnable(){
+        
+    }
+    @Override
+    public void onDisable(){
+        
+    }
+    
+}
